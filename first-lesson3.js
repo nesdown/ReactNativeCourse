d4578145e5e648ef5c5c343c0c4fdafc9786eeba
nesdown <newsdownloads12@gmail.com>
2019-01-17 22:44:59 +0200

    Add third lesson

---
+++ first-lesson3.js
@@ -0,0 +1,19 @@
+'use strict'
+
+let a = 8;
+let b = 10;
+let c = "Hello";
+let d = "Goodbye";
+let f = true;
+
+if (f) {
+  console.log(":)");
+}
+
+else if (a == 9) {
+  console.log(":/");
+}
+
+else {
+  console.log(":(");
+}
